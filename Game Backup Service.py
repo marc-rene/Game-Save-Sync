fc5d9215cf7af887e59135d6f3588db68d87c05e
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-03-26 12:20:12 +0000

    Basics added

--- Game Backup Service.py
+++ Game Backup Service.py
@@ -1,18 +1,44 @@
 import os, time
+from pathlib import Path
+from enum import Enum
+#from nicegui import ui
 
-GSBS_autostart = True if __name__ == "__main__" else False
+AppName = "Game Save Sync"
+class UiNeeded(Enum):
+    not_needed = 1
+    appdata_permission_error = 2
+    no_config_detected = 3
+    
+isUiNeeded = UiNeeded.not_needed
 
-# - - - - - - - - - - - Folder Paths - - - - - - - - - - - - #
-GSBS_Onedrive_Location = "C:\Users\cesar\OneDrive\Saved Games"
+# Step 1: Check and see if we have any config file in APPDATA?
+# Windows implementation
+AppDataLocal = Path(os.getenv('LOCALAPPDATA')) / AppName
 
-GSBS_LocalSavePaths = {
-    "C:\Users\cesar\AppData\Local\Colossal Order\Cities_Skylines\Saves"
-}
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+print(f"Appdata folder is {AppDataLocal}")
+
+try:
+    AppDataLocal.mkdir(parents=True, exist_ok=True)
+    config_file = AppDataLocal / "config.json"
+    
+    if not config_file.exists():
+        isUiNeeded = UiNeeded.no_config_detected
+    
+except PermissionError:
+    print(f"""  We do NOT have permission to access our config folder in {AppDataLocal}
+                Please can you allow {AppName} the permission to make a config?""")
+    isUiNeeded = UiNeeded.appdata_permission_error
 
 
-while True:
-    for game in GSBS_LocalSavePaths:
-        local_saves = [f for f in os.listdir(game) if os.isfile(os.join(game, f))]
+
+match(isUiNeeded):
+    case UiNeeded.not_needed:
+        print(f"All is good, running {AppName}")
         
+    case UiNeeded.no_config_detected:
+        print(f"{config_file} does not exist, running UI to make one")
+
+    case UiNeeded.appdata_permission_error:
+        print(f"We dont have permission to make {config_file} in {AppDataLocal}")
+
     
